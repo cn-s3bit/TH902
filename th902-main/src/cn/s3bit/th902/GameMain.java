97c6f5aa3b6b6abedfb9db287fdd5d8fd4b76d76
北海若 <shiruoxi2010@163.com>
2017-11-03 20:38:12 +0800

    Create Main Project

---
+++ th902-main/src/cn/s3bit/th902/GameMain.java
@@ -0,0 +1,18 @@
+package cn.s3bit.th902;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * @author Obsidianss
+ * <p>
+ * Game class.
+ * </p>
+ */
+public class GameMain extends Game {
+	public static final String GAME_TITLE = "TH902";
+	@Override
+	public void create() {
+		
+	}
+
+}
---
+++ th902-main/src/cn/s3bit/th902/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Modify Logs:
+ * 2017.11.3 Obsidianss: Create Main Project
+ */
+/**
+ * @author Obsidianss
+ * <p>
+ * Main package of the game.
+ * </p>
+ */
+package cn.s3bit.th902;
