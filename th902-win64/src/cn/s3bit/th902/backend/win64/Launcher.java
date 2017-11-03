931a6dc0ecf189e9e88d781c8f8399b12b265cbb
北海若 <shiruoxi2010@163.com>
2017-11-03 20:38:22 +0800

    Create Win64 Platform Project

---
+++ th902-win64/src/cn/s3bit/th902/backend/win64/Launcher.java
@@ -0,0 +1,35 @@
+package cn.s3bit.th902.backend.win64;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import cn.s3bit.th902.GameMain;
+
+/**
+ * @author Obsidianss
+ * <p>
+ * The launcher of the game and the wrapper of LwjglApplication.
+ * </p>
+ */
+public class Launcher {
+	/**
+	 * The {@link LwjglApplication} instance.
+	 */
+	public static LwjglApplication lwjglApplication;
+	/**
+	 * Entry point.
+	 */
+	public static void main(String[] args) {
+		Game game = new GameMain();
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.fullscreen = false;
+		config.width = 960;
+		config.width = 720;
+		config.resizable = false;
+		config.title = GameMain.GAME_TITLE;
+		config.addIcon("resources/icon32.png", FileType.Internal);
+		lwjglApplication = new LwjglApplication(game, config);
+	}
+}
---
+++ th902-win64/src/cn/s3bit/th902/backend/win64/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Modify Logs:
+ * 2017.11.3 Obsidianss: Create Win64 Project
+ */
+/**
+ * @author Obsidianss
+ * <p>
+ * The Win64 backend of the project.
+ * </p>
+ */
+package cn.s3bit.th902.backend.win64;
