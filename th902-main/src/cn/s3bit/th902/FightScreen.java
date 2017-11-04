97689ccec2c388d44fd36da3ecda278354b2890c
swordarrow2 <2856986197@qq.com>
2017-11-05 03:20:29 +0800

    Add a player Reimu(no slow move or shoot)

--- th902-main/src/cn/s3bit/th902/FightScreen.java
+++ th902-main/src/cn/s3bit/th902/FightScreen.java
@@ -1,24 +1,44 @@
 package cn.s3bit.th902;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 import cn.s3bit.th902.gamecontents.Entity;
 import cn.s3bit.th902.gamecontents.components.ImageRenderer;
 import cn.s3bit.th902.gamecontents.components.Transform;
+import cn.s3bit.th902.player.PlayerEntity;
+import cn.s3bit.th902.player.PlayerReimu;
 
 public class FightScreen extends ScreenAdapter {
+	private InputMultiplexer InputMgr;
+	private BitmapFont bf;
+	private SpriteBatch sbatch;
+
 	@Override
 	public void show() {
 		super.show();
+		bf = new BitmapFont();
+		sbatch = new SpriteBatch();
 		Entity fightScreenEntity = Entity.Create();
 		fightScreenEntity.AddComponent(new Transform(new Vector2(480, 360)));
 		fightScreenEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("FightScreen"), 0));
+		new PlayerReimu().PlayerInit();
+		InputMgr = new InputMultiplexer();
+		InputMgr.addProcessor(new PlayerEntity.PlayerInputProcessor());
+		Gdx.input.setInputProcessor(InputMgr);
 	}
-	
+
 	@Override
 	public void render(float delta) {
 		Entity.UpdateAll();
+		PlayerEntity.PlayerEntityInstance.PlayerUpdate();
+		sbatch.begin();
+		bf.draw(sbatch, "FPS:" + Gdx.graphics.getFramesPerSecond(), 20, 35);
+		sbatch.end();
 		super.render(delta);
 	}
 }
--- th902-main/src/cn/s3bit/th902/KeySettings.java
+++ th902-main/src/cn/s3bit/th902/KeySettings.java
@@ -3,11 +3,11 @@ package cn.s3bit.th902;
 import com.badlogic.gdx.Input.Keys;
 
 public final class KeySettings {
-	public static int positiveKey = Keys.Z;
-	public static int negativeKey = Keys.X;
-	public static int up = Keys.UP;
-	public static int down = Keys.DOWN;
-	public static int left = Keys.LEFT;
-	public static int right = Keys.RIGHT;
-	public static int shift=Keys.SHIFT_LEFT;
+	public static final int positiveKey = Keys.Z;
+	public static final int negativeKey = Keys.X;
+	public static final int up = Keys.UP;
+	public static final int down = Keys.DOWN;
+	public static final int left = Keys.LEFT;
+	public static final int right = Keys.RIGHT;
+	public static final int shift=Keys.SHIFT_LEFT;
 }
