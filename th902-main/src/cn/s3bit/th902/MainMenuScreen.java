d4cd5ff5e2f7615747348ea0c55f2d8eea78faf0
ALINGCAT <2218773842@qq.com>
2017-11-04 13:23:22 +0800

    Add five backgrounds and a "Story Mode" button

--- th902-main/src/cn/s3bit/th902/MainMenuScreen.java
+++ th902-main/src/cn/s3bit/th902/MainMenuScreen.java
@@ -11,12 +11,51 @@ public class MainMenuScreen extends ScreenAdapter {
 	@Override
 	public void show() {
 		super.show();
-		//TODO Add UI Entities Here
-		Entity exampleEntity = Entity.Create();
-		exampleEntity.AddComponent(new Transform(new Vector2(128, 128), new Vector2(4, 4)));
-		exampleEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("icon32"), 0));
+		// TODO Add UI Entities Here
+		Entity backgroundEntity = Entity.Create();
+		Entity button1Entity = Entity.Create();
+		byte select = (byte) (Math.random() * 5 + 1);
+		//select = 5;
+		switch (select) {
+		case 1:
+			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
+			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background1"), 0));
+			button1Entity.AddComponent(new Transform(new Vector2(620, 240)));
+			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
+			break;
+		case 2:
+			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
+			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background2"), 0));
+			button1Entity.AddComponent(new Transform(new Vector2(300, 650)));
+			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
+			break;
+		case 3:
+			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
+			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background3"), 0));
+			button1Entity.AddComponent(new Transform(new Vector2(280, 630)));
+			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
+			break;
+		case 4:
+			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
+			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background4"), 0));
+			button1Entity.AddComponent(new Transform(new Vector2(900, 650)));
+			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
+			break;
+		case 5:
+			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
+			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background5"), 0));
+			button1Entity.AddComponent(new Transform(new Vector2(870, 600)));
+			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
+			break;
+		default:
+			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
+			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background1"), 0));
+			button1Entity.AddComponent(new Transform(new Vector2(620, 240)));
+			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
+			break;
+		}
 	}
-	
+
 	@Override
 	public void render(float delta) {
 		Entity.UpdateAll();
--- th902-main/src/cn/s3bit/th902/ResourceManager.java
+++ th902-main/src/cn/s3bit/th902/ResourceManager.java
@@ -10,7 +10,12 @@ public final class ResourceManager {
 	public static HashMap<String, Texture> textures = new HashMap<>();
 	public static void Load() {
 		textures.clear();
-		AddTexture("icon32", "resources/icon32.png", FileType.Internal);
+		AddTexture("Background1", "resources/Background1.png", FileType.Internal);
+		AddTexture("Background2", "resources/Background2.png", FileType.Internal);
+		AddTexture("Background3", "resources/Background3.png", FileType.Internal);
+		AddTexture("Background4", "resources/Background4.png", FileType.Internal);
+		AddTexture("Background5", "resources/Background5.png", FileType.Internal);
+		AddTexture("Button1", "resources/Button1.png", FileType.Internal);
 	}
 	public static void AddTexture(String name, String path, FileType type) {
 		textures.put(name, new Texture(Gdx.files.getFileHandle(path, type)));
