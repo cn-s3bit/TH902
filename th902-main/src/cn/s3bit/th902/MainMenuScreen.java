e29c2cf975b13773634d8430cb44d214e51535d7
ALINGCAT <2218773842@qq.com>
2017-11-04 14:09:22 +0800

    Make menu code easy and change the image of button "Story Mode"

--- th902-main/src/cn/s3bit/th902/MainMenuScreen.java
+++ th902-main/src/cn/s3bit/th902/MainMenuScreen.java
@@ -15,45 +15,29 @@ public class MainMenuScreen extends ScreenAdapter {
 		Entity backgroundEntity = Entity.Create();
 		Entity button1Entity = Entity.Create();
 		byte select = (byte) (Math.random() * 5 + 1);
-		//select = 5;
+		// select = 5;
 		switch (select) {
-		case 1:
-			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
-			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background1"), 0));
-			button1Entity.AddComponent(new Transform(new Vector2(620, 240)));
-			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
-			break;
 		case 2:
-			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
-			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background2"), 0));
 			button1Entity.AddComponent(new Transform(new Vector2(300, 650)));
-			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
 			break;
 		case 3:
-			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
-			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background3"), 0));
 			button1Entity.AddComponent(new Transform(new Vector2(280, 630)));
-			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
 			break;
 		case 4:
-			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
-			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background4"), 0));
 			button1Entity.AddComponent(new Transform(new Vector2(900, 650)));
-			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
 			break;
 		case 5:
-			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
-			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background5"), 0));
 			button1Entity.AddComponent(new Transform(new Vector2(870, 600)));
-			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
 			break;
 		default:
-			backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
-			backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background1"), 0));
 			button1Entity.AddComponent(new Transform(new Vector2(620, 240)));
-			button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
 			break;
 		}
+		backgroundEntity.AddComponent(new Transform(new Vector2(480, 360)));
+		for (byte i = 1; i <= 5; i++)
+			if (i == select)
+				backgroundEntity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Background" + i), 0));
+		button1Entity.AddComponent(new ImageRenderer(ResourceManager.textures.get("Button1"), 1));
 	}
 
 	@Override
