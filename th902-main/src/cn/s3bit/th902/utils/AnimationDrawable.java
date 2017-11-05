e07693b609d48317fb5919e9b25449eb4ddc6553
北海若 <shiruoxi2010@163.com>
2017-11-05 09:56:43 +0800

    AnimationDrawable

---
+++ th902-main/src/cn/s3bit/th902/utils/AnimationDrawable.java
@@ -0,0 +1,22 @@
+package cn.s3bit.th902.utils;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+public class AnimationDrawable extends TextureRegionDrawable {
+	private Animation<TextureRegion> mAnimation;
+	float currentTime = 0;
+	public void advance(float delta) {
+		currentTime += delta;
+		setRegion(mAnimation.getKeyFrame(currentTime, false));
+	}
+	public void setAnimation(Animation<TextureRegion> animation) {
+		mAnimation = animation;
+		currentTime = 0;
+		setRegion(mAnimation.getKeyFrame(currentTime));
+	}
+	public Animation<TextureRegion> getAnimation() {
+		return mAnimation;
+	}
+}
