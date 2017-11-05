1707ce09bfdcb6dda97ed17e8dc872fc5b090d0f
北海若 <shiruoxi2010@163.com>
2017-11-05 14:58:32 +0800

    Add Drawable Constructor to ImageRenderer

--- th902-main/src/cn/s3bit/th902/gamecontents/components/ImageRenderer.java
+++ th902-main/src/cn/s3bit/th902/gamecontents/components/ImageRenderer.java
@@ -2,6 +2,7 @@ package cn.s3bit.th902.gamecontents.components;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Align;
 
 import cn.s3bit.th902.GameMain;
@@ -20,6 +21,17 @@ public class ImageRenderer extends Component {
 	public ImageRenderer(Texture texture, int depth) {
 		image = new Image(texture);
 		image.setBounds(0, 0, texture.getWidth(), texture.getHeight());
+		mDepth = depth;
+	}
+	/**
+	 * @param texture The Drawable Texture.
+	 * @param depth -1 for the front, 0 for the back.
+	 *              Otherwise it is an index from 0 to count(Images) - 1.
+	 *              A larger index is in the front of the smaller.
+	 */
+	public ImageRenderer(Drawable texture, int depth) {
+		image = new Image(texture);
+		image.setBounds(0, 0, texture.getMinWidth(), texture.getMinHeight());
 		mDepth = depth;
 	}
 	
