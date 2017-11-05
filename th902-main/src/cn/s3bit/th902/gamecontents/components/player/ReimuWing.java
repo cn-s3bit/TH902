70257383a6d509a7fbb0bc453c02f52f6f6f6f3c
?????·è?? <shiruoxi2010@163.com>
2017-11-05 16:51:52 +0800

    Original proj pictures of 65 & 67 are now 234 & 235 add 236

---
+++ th902-main/src/cn/s3bit/th902/gamecontents/components/player/ReimuWing.java
@@ -0,0 +1,21 @@
+package cn.s3bit.th902.gamecontents.components.player;
+
+import cn.s3bit.th902.gamecontents.Entity;
+import cn.s3bit.th902.gamecontents.components.Component;
+
+public class ReimuWing extends Component {
+
+	@Override
+	public void Initialize(Entity entity) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void Update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
