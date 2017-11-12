805903f71213bd6c36105def5378de5de011f6c9
ALINGCAT <2218773842@qq.com>
2017-11-12 13:25:04 +0800

    Add relation of images and players/bombs

--- th902-main/src/cn/s3bit/th902/FightScreen.java
+++ th902-main/src/cn/s3bit/th902/FightScreen.java
@@ -14,9 +14,11 @@ public class FightScreen extends ScreenAdapter {
 	private BitmapFont bf;
 	private int _difficulty = DifficultySelectScreen.difficulty;
 	private String mDifficulty[] = { "Easy", "Normal", "Hard", "Lunatic" };
-	public static int gameTime=0;
-	public static int playerCount=2;
-	public static int bombCount=3;
+	private ImageRenderer mBombs[];
+	private ImageRenderer mHearts[];
+	public static int gameTime = 0;
+	public static int playerCount = 2;
+	public static int bombCount = 3;
 
 	@Override
 	public void show() {
@@ -32,8 +34,8 @@ public class FightScreen extends ScreenAdapter {
 		Entity power = Entity.Create();
 		Entity players = Entity.Create();
 		Entity spellCard = Entity.Create();
-		Entity bomb = Entity.Create();
-		Entity heart = Entity.Create();
+		Entity bombs[] = new Entity[8];
+		Entity hearts[] = new Entity[8];
 
 		fightScreen.AddComponent(new Transform(new Vector2(480, 360)));
 		player.AddComponent(new Transform(new Vector2(240, 160), new Vector2(0.4f, 0.4f)));
@@ -45,8 +47,6 @@ public class FightScreen extends ScreenAdapter {
 		power.AddComponent(new Transform(new Vector2(810, 350)));
 		players.AddComponent(new Transform(new Vector2(810, 500)));
 		spellCard.AddComponent(new Transform(new Vector2(615, 450)));
-		bomb.AddComponent(new Transform(new Vector2(720.5f, 450.5f)));
-		heart.AddComponent(new Transform(new Vector2(720.5f, 500.5f)));
 
 		fightScreen.AddComponent(new ImageRenderer(ResourceManager.textures.get("FightScreen"), 0));
 		if (_difficulty > 0 && _difficulty < 5)
@@ -54,13 +54,30 @@ public class FightScreen extends ScreenAdapter {
 		score.AddComponent(new ImageRenderer(ResourceManager.textures.get("Score"), 1));
 		highScore.AddComponent(new ImageRenderer(ResourceManager.textures.get("HighScore"), 1));
 		players.AddComponent(new ImageRenderer(ResourceManager.textures.get("Players"), 1));
-		heart.AddComponent(new ImageRenderer(ResourceManager.textures.get("Heart"), 1));
 		spellCard.AddComponent(new ImageRenderer(ResourceManager.textures.get("SpellCard"), 1));
-		bomb.AddComponent(new ImageRenderer(ResourceManager.textures.get("Bomb"), 1));
 		power.AddComponent(new ImageRenderer(ResourceManager.textures.get("Power"), 1));
 		graze.AddComponent(new ImageRenderer(ResourceManager.textures.get("Graze"), 1));
 		point.AddComponent(new ImageRenderer(ResourceManager.textures.get("Point"), 1));
 		player.AddComponent(new PlayerReimu());
+
+		for (int i = 0; i < 8; i++) {
+
+			bombs[i] = Entity.Create();
+			hearts[i] = Entity.Create();
+			bombs[i].AddComponent(new Transform(new Vector2(720.5f + 30.0f * i, 450.5f)));
+			hearts[i].AddComponent(new Transform(new Vector2(720.5f + 30.0f * i, 500.5f)));
+			bombs[i].AddComponent(new ImageRenderer(ResourceManager.textures.get("Bomb"), 1));
+			hearts[i].AddComponent(new ImageRenderer(ResourceManager.textures.get("Heart"), 1));
+			mBombs[i] = bombs[i].GetComponent(ImageRenderer.class);
+			mHearts[i] = hearts[i].GetComponent(ImageRenderer.class);
+			mBombs[i].image.setColor(1, 1, 1, 0);
+			mHearts[i].image.setColor(1, 1, 1, 0);
+		}
+
+		for (int i = 0; i < playerCount; i++)
+			mHearts[i].image.setColor(1, 1, 1, 1);
+		for (int i = 0; i < bombCount; i++)
+			mBombs[i].image.setColor(1, 1, 1, 1);
 	}
 
 	@Override
@@ -69,27 +86,24 @@ public class FightScreen extends ScreenAdapter {
 		super.render(delta);
 		gameTime++;
 		GameMain.instance.activeStage.getBatch().begin();
-		bf.draw(GameMain.instance.activeStage.getBatch(), "FPS: " + Gdx.graphics.getFramesPerSecond()
-		+"\ndifficulty:"+DifficultySelectScreen.difficulty
-		+"\nplayer:"+playerCount
-		+"\nbomb:"+bombCount
-		, 20, 705);
+		bf.draw(GameMain.instance.activeStage.getBatch(), "FPS: " + Gdx.graphics.getFramesPerSecond() + "\ndifficulty:"
+				+ DifficultySelectScreen.difficulty + "\nplayer:" + playerCount + "\nbomb:" + bombCount, 20, 705);
 		GameMain.instance.activeStage.getBatch().end();
 
 		switch (gameTime) {
-        case 30:
-        	BaseSprite.Create(new Vector2(000,400), 0);
-            break;
-        case 60:
-        	BaseSprite.Create(new Vector2(100,400), 1);
-            break;
-        case 120:
-        	BaseSprite.Create(new Vector2(150,300), 2);
-            break;
-        case 180:
-        	BaseSprite.Create(new Vector2(700,350), 3);
-        	gameTime=0;
-        	break;
+		case 30:
+			BaseSprite.Create(new Vector2(000, 400), 0);
+			break;
+		case 60:
+			BaseSprite.Create(new Vector2(100, 400), 1);
+			break;
+		case 120:
+			BaseSprite.Create(new Vector2(150, 300), 2);
+			break;
+		case 180:
+			BaseSprite.Create(new Vector2(250, 350), 3);
+			gameTime = 0;
+			break;
 		}
 	}
 }
