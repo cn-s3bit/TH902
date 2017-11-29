ee61ad7c5166857164547a784b6e1e27746787f2
swordarrow2 <2856986197@qq.com>
2017-11-30 01:35:48 +0800

    Add new render method

--- th902-main/src/cn/s3bit/th902/gamecontents/components/player/ReimuWing.java
+++ th902-main/src/cn/s3bit/th902/gamecontents/components/player/ReimuWing.java
@@ -1,6 +1,5 @@
 package cn.s3bit.th902.gamecontents.components.player;
 
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 import cn.s3bit.th902.FightScreen;
@@ -14,11 +13,10 @@ public class ReimuWing extends Component {
 
 	protected Vector2 mVector2 = new Vector2();
 	protected int existTime;
-	protected boolean ifSlow = false;
+	protected boolean isSlow = false;
 	protected Transform transform;
 	private float mRotationFlag;
-	protected boolean ifShoot = false;
-	protected float bulletRnd = 0;
+	protected boolean isShooting = false;
 	private int mType;
 
 	public ReimuWing(Vector2 position, float rotation, int type) {
@@ -37,44 +35,29 @@ public class ReimuWing extends Component {
 
 	public void set(Vector2 v, boolean ifSlow, boolean ifShoot) {
 		mVector2 = v;
-		this.ifSlow = ifSlow;
-		this.ifShoot = ifShoot;
+		this.isSlow = ifSlow;
+		this.isShooting = ifShoot;
 	}
 
 	@Override
 	public void Update() {
 		transform.position.add(mVector2.sub(transform.position).scl(0.2f));
 		existTime++;
+		transform.rotation = isShooting ? transform.rotation + mRotationFlag * 3 : mRotationFlag;
 		if (mType == FightScreen.PlayerTypeA) {
-			if (ifSlow) {
-				if (ifShoot) {
-					transform.rotation = transform.rotation + mRotationFlag * 3;
-				} else {
-					transform.rotation += mRotationFlag;
-				}
-			} else {
+			if (!isSlow) {
 				transform.rotation += mRotationFlag;
-				if (ifShoot) {
+				if (isShooting) {
 					ReimuBullet1.Create(transform.position.cpy().add(0, 6), ReimuBullet1.BulletTypeWingFastInduce);
 				}
 			}
 		} else {
-			if (ifSlow) {
-				if (ifShoot) {
-					transform.rotation = transform.rotation + mRotationFlag * 3;
-				} else {
-					transform.rotation += mRotationFlag;
-				}
-			} else {
+			if (!isSlow) {
 				transform.rotation += mRotationFlag;
-				if (ifShoot) {
-					bulletRnd = MathUtils.random(-6, 6);
-					ReimuBullet1.Create(transform.position.cpy().add(-10, bulletRnd),
-							ReimuBullet1.BulletTypeWingFastStraight);
-					ReimuBullet1.Create(transform.position.cpy().add(0, bulletRnd + 6),
-							ReimuBullet1.BulletTypeWingFastStraight);
-					ReimuBullet1.Create(transform.position.cpy().add(10, bulletRnd),
-							ReimuBullet1.BulletTypeWingFastStraight);
+				if (isShooting) {
+					ReimuBullet1.Create(transform.position.cpy().add(-15, 6), ReimuBullet1.BulletTypeWingFastStraight);
+					ReimuBullet1.Create(transform.position.cpy().add(0, 12), ReimuBullet1.BulletTypeWingFastStraight);
+					ReimuBullet1.Create(transform.position.cpy().add(15, 6), ReimuBullet1.BulletTypeWingFastStraight);
 				}
 			}
 		}
