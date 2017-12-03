8593d48bdc52c75363b8b0a71ca1080198c91d10
北海若 <shiruoxi2010@163.com>
2017-12-03 12:38:21 +0800

    A Small Change to keep the particles under the background

--- th902-main/src/cn/s3bit/th902/GameMain.java
+++ th902-main/src/cn/s3bit/th902/GameMain.java
@@ -45,7 +45,7 @@ public class GameMain extends Game {
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		activeStage.draw();
 		super.render();
+		activeStage.draw();
 	}
 }
