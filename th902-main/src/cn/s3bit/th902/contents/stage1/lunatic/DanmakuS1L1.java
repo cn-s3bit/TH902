000dc469ae48f5236ee52c7c36865a10a6828161
?????·è?? <shiruoxi2010@163.com>
2017-12-08 21:30:26 +0800

    Zombie Bullets

--- th902-main/src/cn/s3bit/th902/contents/stage1/lunatic/DanmakuS1L1.java
+++ th902-main/src/cn/s3bit/th902/contents/stage1/lunatic/DanmakuS1L1.java
@@ -5,13 +5,15 @@ import com.badlogic.gdx.math.Vector2;
 
 import cn.s3bit.th902.gamecontents.DanmakuScene;
 import cn.s3bit.th902.gamecontents.Entity;
-import cn.s3bit.th902.gamecontents.components.LambdaComponent;
+import cn.s3bit.th902.gamecontents.JudgingSystem;
+import cn.s3bit.th902.gamecontents.components.ExtraDrop;
 import cn.s3bit.th902.gamecontents.components.Transform;
 import cn.s3bit.th902.gamecontents.components.ai.MoveBasic;
 import cn.s3bit.th902.gamecontents.components.ai.MoveSnipe;
 import cn.s3bit.th902.gamecontents.components.enemy.BaseProjectile;
 import cn.s3bit.th902.gamecontents.components.enemy.BaseSprite;
 import cn.s3bit.th902.gamecontents.components.enemy.BulletType;
+import cn.s3bit.th902.gamecontents.components.enemy.DropItem;
 import cn.s3bit.th902.gamecontents.components.enemy.EnemyJudgeCircle;
 
 public class DanmakuS1L1 extends DanmakuScene {
@@ -26,23 +28,32 @@ public class DanmakuS1L1 extends DanmakuScene {
 				Entity sprite = BaseSprite.Create(new Vector2(285 - 0.5f * 270, 730), 3, 20);
 				final Transform transform1 = sprite.GetComponent(Transform.class);
 				sprite.AddComponent(new MoveBasic(0.5f, -3, 0, -0.0001f));
-				sprite.AddComponent(new LambdaComponent(() -> {
-					Entity proj = BaseProjectile.Create(transform1.position.cpy(), BulletType.FormCircleS, BulletType.ColorRed);
-					proj.AddComponent(new MoveSnipe(4));
-					proj.AddComponent(new EnemyJudgeCircle(6));
-				}, 30, -1));
+				
+				sprite.AddComponent(new ExtraDrop() {
+					@Override
+					public void LootLogic() {
+						DropItem.CreateDropItem(transform1.position.cpy(), 241 + MathUtils.random(1));
+						Entity proj = BaseProjectile.Create(transform1.position.cpy(), BulletType.FormCircleS, BulletType.ColorRed);
+						proj.AddComponent(new MoveSnipe(4));
+						proj.AddComponent(new EnemyJudgeCircle(6));
+					}
+				});
 				
 				sprite = BaseSprite.Create(new Vector2(285 + 0.5f * 270, 730), 3, 20);
 				final Transform transform2 = sprite.GetComponent(Transform.class);
 				sprite.AddComponent(new MoveBasic(-0.5f, -3, 0, -0.0001f));
-				sprite.AddComponent(new LambdaComponent(() -> {
-					for (int j=0; j<6; j++)
-					{
-						Entity proj = BaseProjectile.Create(transform2.position.cpy(), BulletType.FormCircleS, BulletType.ColorRed);
-						proj.AddComponent(new MoveBasic(MathUtils.random(-3f, 3f), MathUtils.random(-3f, 3f)));
-						proj.AddComponent(new EnemyJudgeCircle(6));
+				sprite.AddComponent(new ExtraDrop() {
+					@Override
+					public void LootLogic() {
+						for (int j=0; j<6; j++)
+						{
+							DropItem.CreateDropItem(transform2.position.cpy(), 241 + MathUtils.random(1));
+							Entity proj = BaseProjectile.Create(transform2.position.cpy(), BulletType.FormCircleS, BulletType.ColorRed);
+							proj.AddComponent(new MoveBasic(MathUtils.random(-3f, 3f), MathUtils.random(-3f, 3f)));
+							proj.AddComponent(new EnemyJudgeCircle(6));
+						}
 					}
-				}, 30, -1));
+				});
 			});
 		}
 	}
