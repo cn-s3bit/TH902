abe88a4f58969d69a67d53a436ec9fb25d7f7b1b
北海若 <shiruoxi2010@163.com>
2017-12-10 09:19:27 +0800

    DanmakuS1L3

---
+++ th902-main/src/cn/s3bit/th902/contents/stage1/lunatic/DanmakuS1L3.java
@@ -0,0 +1,55 @@
+package cn.s3bit.th902.contents.stage1.lunatic;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+import cn.s3bit.th902.gamecontents.DanmakuScene;
+import cn.s3bit.th902.gamecontents.Entity;
+import cn.s3bit.th902.gamecontents.JudgingSystem;
+import cn.s3bit.th902.gamecontents.components.ExtraDrop;
+import cn.s3bit.th902.gamecontents.components.LambdaComponent;
+import cn.s3bit.th902.gamecontents.components.Transform;
+import cn.s3bit.th902.gamecontents.components.ai.MoveBasic;
+import cn.s3bit.th902.gamecontents.components.enemy.BaseProjectile;
+import cn.s3bit.th902.gamecontents.components.enemy.BaseSprite;
+import cn.s3bit.th902.gamecontents.components.enemy.BulletType;
+import cn.s3bit.th902.gamecontents.components.enemy.DropItem;
+import cn.s3bit.th902.gamecontents.components.enemy.EnemyJudgeCircle;
+
+public class DanmakuS1L3 extends DanmakuScene {
+	int count = 0;
+	@Override
+	public void Initialize(Entity entity) {
+		yield.append(() -> {}, 60);
+		for (int i = 1; i <= 120; i++) {
+			yield.append(() -> {}, 17 - i / 9);
+			yield.append(() -> {
+				int dir = MathUtils.randomSign();
+				Vector2 pos = new Vector2(dir * 300 + 285, MathUtils.random(500, 570));
+				final Entity sprite = BaseSprite.Create(pos, 2, 8, new MoveBasic(-10 * dir, MathUtils.random(-3f, 0f)));
+				final Transform transform = sprite.GetComponent(Transform.class);
+				sprite.AddComponent(new LambdaComponent(() -> {
+					for (int j=0; j<5; j++)
+					{
+						Entity proj = BaseProjectile.Create(transform.position.cpy(), BulletType.FormCircleS, BulletType.ColorRed);
+						proj.AddComponent(new MoveBasic(MathUtils.random(-3f, 3f), MathUtils.random(-3f, -1f) * (JudgingSystem.playerJudge.y > transform.position.y ? -1 : 1)));
+						proj.AddComponent(new EnemyJudgeCircle(6));
+						proj = BaseProjectile.Create(transform.position.cpy(), BulletType.FormCircleS, BulletType.ColorRed);
+						proj.AddComponent(new MoveBasic(MathUtils.random(-3f, 3f), MathUtils.random(-3f, -1f) * (JudgingSystem.playerJudge.y > transform.position.y ? -1 : 1)));
+						proj.AddComponent(new EnemyJudgeCircle(6));
+					}
+				}, MathUtils.random(8, 30), -1));
+				sprite.AddComponent(new ExtraDrop() {
+					@Override
+					public void LootLogic() {
+						count++;
+						if (count % 3 == 0) {
+							DropItem.CreateDropItem(transform.position.cpy(), 242);
+						}
+					}
+				});
+			});
+		}
+	}
+
+}
