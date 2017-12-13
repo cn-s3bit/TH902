98eba165be14eaf203c854c3a1fa9557c7faa939
北海若 <shiruoxi2010@163.com>
2017-12-13 17:47:33 +0800

    S1L4 Boss

--- th902-main/src/cn/s3bit/th902/contents/stage1/lunatic/AIS1L4Boss.java
+++ th902-main/src/cn/s3bit/th902/contents/stage1/lunatic/AIS1L4Boss.java
@@ -1,29 +1,75 @@
 package cn.s3bit.th902.contents.stage1.lunatic;
 
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
 import cn.s3bit.th902.gamecontents.Entity;
 import cn.s3bit.th902.gamecontents.components.Component;
 import cn.s3bit.th902.gamecontents.components.Transform;
+import cn.s3bit.th902.gamecontents.components.ai.MoveBasic;
 import cn.s3bit.th902.gamecontents.components.enemy.BaseProjectile;
-import cn.s3bit.th902.gamecontents.components.enemy.BulletType;
+import cn.s3bit.th902.gamecontents.components.enemy.BossHP;
+import cn.s3bit.th902.gamecontents.components.enemy.EnemyJudgeCircle;
 
 public class AIS1L4Boss extends Component {
 	Transform transform;
+	BossHP bossHp;
 	@Override
 	public void Initialize(Entity entity) {
 		transform = entity.GetComponent(Transform.class);
+		bossHp = entity.GetComponent(BossHP.class);
 	}
 
 	public int batch = 0;
 	public int existTime = 0;
+	Vector2 vct2_tmp = new Vector2();
 	@Override
 	public void Update() {
 		existTime++;
-		if (existTime % 20 == 0) {
+		switch (bossHp.current) {
+		case 0:
+			part1();
+			break;
+		case 1:
+			part2();
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public void part1() {
+		if (existTime % 10 == 0) {
 			batch++;
-			for (int i = batch * 20; i < batch * 20 + 360; i += 20) {
-				//BaseProjectile.Create(transform.position.cpy(), BulletType., colorred, Ves)
+			int co = batch;
+			for (int i = batch * 17; i < batch * 17 + 360; i += 20) {
+				co++;
+				float size = Math.abs(MathUtils.cosDeg(i * 2.5f)) * 0.75f + 0.2f;
+				Entity proj = BaseProjectile.CreateSpecialBullet(transform.position.cpy(), co % 2 == 0 ? 225 : 226);
+				proj.AddComponent(new MoveBasic(vct2_tmp.set(8, 0).rotate(i)));
+				proj.GetComponent(Transform.class).scale.scl(size);
+				proj.AddComponent(new EnemyJudgeCircle(15 * size));
 			}
 		}
 	}
 	
+	public void part2() {
+		if (existTime % 10 == 0) {
+			batch++;
+			for (int i = batch * 17; i < batch * 17 + 360; i += 30) {
+				float size = Math.abs(MathUtils.cosDeg(i * 2.5f)) * 0.75f + 0.2f;
+				Entity proj = BaseProjectile.CreateSpecialBullet(transform.position.cpy(), 225);
+				proj.AddComponent(new MoveBasic(vct2_tmp.set(8, 0).rotate(i)));
+				proj.GetComponent(Transform.class).scale.scl(size);
+				proj.AddComponent(new EnemyJudgeCircle(15 * size));
+			}
+			for (int i = -batch * 17; i < -batch * 17 + 360; i += 60) {
+				float size = 0.5f;
+				Entity proj = BaseProjectile.CreateSpecialBullet(transform.position.cpy(), 226);
+				proj.AddComponent(new MoveBasic(vct2_tmp.set(5, 0).rotate(i)));
+				proj.GetComponent(Transform.class).scale.scl(size);
+				proj.AddComponent(new EnemyJudgeCircle(15 * size));
+			}
+		}
+	}
 }
--- th902-main/src/cn/s3bit/th902/contents/stage1/lunatic/DanmakuS1L4.java
+++ th902-main/src/cn/s3bit/th902/contents/stage1/lunatic/DanmakuS1L4.java
@@ -1,23 +1,17 @@
 package cn.s3bit.th902.contents.stage1.lunatic;
 
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 import cn.s3bit.th902.ResourceManager;
 import cn.s3bit.th902.gamecontents.DanmakuScene;
 import cn.s3bit.th902.gamecontents.Entity;
-import cn.s3bit.th902.gamecontents.JudgingSystem;
 import cn.s3bit.th902.gamecontents.components.ImageRenderer;
-import cn.s3bit.th902.gamecontents.components.LambdaComponent;
 import cn.s3bit.th902.gamecontents.components.Transform;
 import cn.s3bit.th902.gamecontents.components.ai.IMoveFunction;
-import cn.s3bit.th902.gamecontents.components.ai.MoveBasic;
 import cn.s3bit.th902.gamecontents.components.ai.MoveFunction;
 import cn.s3bit.th902.gamecontents.components.ai.MoveFunctionTarget;
 import cn.s3bit.th902.gamecontents.components.ai.MoveFunctionType;
-import cn.s3bit.th902.gamecontents.components.enemy.BaseProjectile;
 import cn.s3bit.th902.gamecontents.components.enemy.BossHP;
-import cn.s3bit.th902.gamecontents.components.enemy.BulletType;
 import cn.s3bit.th902.gamecontents.components.enemy.DropItem;
 import cn.s3bit.th902.gamecontents.components.enemy.EnemyChaseable;
 import cn.s3bit.th902.gamecontents.components.enemy.EnemyJudgeCircle;
@@ -33,22 +27,18 @@ public class DanmakuS1L4 extends DanmakuScene {
 			boss.AddComponent(transform);
 			boss.AddComponent(new ImageRenderer(ResourceManager.barrages.get(230), 0));
 			BossHP hp = new BossHP(
-					new int[]{3600},
-					new int[]{3600},
-					new String[]{"TEST SPELL - [TEST]"},
-					new Runnable[]{null},
-					new Runnable[]{ () -> { DropItem.CreateDropItem(transform.position.cpy(), 241); } });
+					new int[]{1800, 1800},
+					new int[]{2400, 2400},
+					new String[]{"TEST SPELL - [TEST]", null},
+					new Runnable[]{null, null},
+					new Runnable[]{null, () -> { DropItem.CreateDropItem(transform.position.cpy(), 241); } });
 			boss.AddComponent(hp);
 			boss.AddComponent(new EnemyJudgeCircle(54, hp));
 			boss.AddComponent(new EnemyChaseable(hp));
 			boss.AddComponent(new MoveFunction(MoveFunctionTarget.VELOCITY, MoveFunctionType.ASSIGNMENT, (time) -> {
 				return IMoveFunction.vct2_tmp1.set(0, time < 40 ? -4f : 0);
 			}));
-			boss.AddComponent(new LambdaComponent(() -> {
-				BaseProjectile.Create(transform.position.cpy(), BulletType.FormBulletM, BulletType.ColorPink,
-						new MoveBasic(MathUtils.random(-4f, 4f), MathUtils.random(-3f, -1f) * (JudgingSystem.playerJudge.y > transform.position.y ? -1 : 1)),
-						new EnemyJudgeCircle(4));
-			}, 1));
+			boss.AddComponent(new AIS1L4Boss());
 		});
 	}
 
