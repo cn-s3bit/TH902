af8f182fe140c59eb2f480b9022fae4136cde90e
北海若 <shiruoxi2010@163.com>
2017-12-21 13:07:19 +0800

    DanmakuS1Last Class Structure

---
+++ th902-main/src/cn/s3bit/th902/contents/stage1/lunatic/DanmakuS1Last.java
@@ -0,0 +1,13 @@
+package cn.s3bit.th902.contents.stage1.lunatic;
+
+import cn.s3bit.th902.gamecontents.DanmakuScene;
+import cn.s3bit.th902.gamecontents.Entity;
+
+public class DanmakuS1Last extends DanmakuScene {
+
+	@Override
+	public void Initialize(Entity entity) {
+		
+	}
+
+}
