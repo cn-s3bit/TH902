8240519c7915fb0186282a159ef7569541953abb
北海若 <shiruoxi2010@163.com>
2017-12-24 18:01:47 +0800

    S1Lunatic Last Boss Part1 Structure

--- th902-main/src/cn/s3bit/th902/contents/stage1/lunatic/AIS1LLastBoss.java
+++ th902-main/src/cn/s3bit/th902/contents/stage1/lunatic/AIS1LLastBoss.java
@@ -1,20 +1,37 @@
 package cn.s3bit.th902.contents.stage1.lunatic;
 
+import com.badlogic.gdx.math.Vector2;
+
 import cn.s3bit.th902.gamecontents.Entity;
 import cn.s3bit.th902.gamecontents.components.Component;
+import cn.s3bit.th902.gamecontents.components.Transform;
+import cn.s3bit.th902.gamecontents.components.enemy.BossHP;
 
 public class AIS1LLastBoss extends Component {
-
+	Transform transform;
+	BossHP bossHp;
 	@Override
 	public void Initialize(Entity entity) {
-		// TODO Auto-generated method stub
-
+		transform = entity.GetComponent(Transform.class);
+		bossHp = entity.GetComponent(BossHP.class);
 	}
 
+	public int batch = 0;
+	public int existTime = 0;
+	Vector2 vct2_tmp = new Vector2();
 	@Override
 	public void Update() {
-		// TODO Auto-generated method stub
-
+		existTime++;
+		switch (bossHp.current) {
+		case 0:
+			part1();
+			break;
+		default:
+			break;
+		}
 	}
 
+	public void part1() {
+		
+	}
 }
