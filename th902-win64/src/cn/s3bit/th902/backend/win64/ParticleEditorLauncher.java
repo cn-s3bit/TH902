e3775cfa93669a9a1a1a688f1f11fd77c20fc3a6
北海若 <shiruoxi2010@163.com>
2018-01-05 19:49:27 +0800

    ParticleEditorLauncher

---
+++ th902-win64/src/cn/s3bit/th902/backend/win64/ParticleEditorLauncher.java
@@ -0,0 +1,17 @@
+package cn.s3bit.th902.backend.win64;
+
+import com.badlogic.gdx.tools.particleeditor.ParticleEditor;
+
+/**
+ * @author Obsidianss
+ * <p>
+ * Launches {@link ParticleEditor}.
+ * </p>
+ */
+public class ParticleEditorLauncher {
+
+	public static void main(String[] args) {
+		ParticleEditor.main(args);
+	}
+
+}
