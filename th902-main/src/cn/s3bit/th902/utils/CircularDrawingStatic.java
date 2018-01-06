78e616ebbfe3f783091f37507e3a40a9f529a5ea
北海若 <shiruoxi2010@163.com>
2018-01-06 11:57:40 +0800

    Circular Drawing Util Finished

--- th902-main/src/cn/s3bit/th902/utils/CircularDrawingStatic.java
+++ th902-main/src/cn/s3bit/th902/utils/CircularDrawingStatic.java
@@ -1,50 +1,112 @@
 package cn.s3bit.th902.utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Mesh.VertexDataType;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 import cn.s3bit.th902.GameMain;
 import cn.s3bit.th902.ResourceManager;
 
 public class CircularDrawingStatic extends Actor {
-	static Mesh mesh = null;
-	static ShaderProgram shaderProgram = null;
-	public CircularDrawingStatic() {
-	}
-	public static void testRender() {
-		if (mesh == null) {
-			List<VertexAttribute> list = new ArrayList<>();
-			final int n_vtx = 6;
-			list.add(VertexAttribute.Position());
-			list.add(VertexAttribute.ColorUnpacked());
-			list.add(VertexAttribute.TexCoords(0));
-			mesh = new Mesh(VertexDataType.VertexArray, true, n_vtx, n_vtx, list.toArray(new VertexAttribute[list.size()]));
-			shaderProgram = SpriteBatch.createDefaultShader();
+	Mesh mesh = null;
+	ShaderProgram shaderProgram = null;
+	Texture texture = null;
+	float[] initialVertices = null;
+	float[] operatedVertices = null;
+	public CircularDrawingStatic(Texture texture, int beginDegrees, int endDegrees) {
+		this.texture = texture;
+		List<VertexAttribute> list = new ArrayList<>();
+		list.add(VertexAttribute.Position());
+		list.add(VertexAttribute.ColorUnpacked());
+		list.add(VertexAttribute.TexCoords(0));
+		
+		List<Float> lvert = new ArrayList<>(30);
+		int n_vtx = 0;
+		lvert.addAll(Arrays.asList(0f, 0f, 0f, 1f, 1f, 1f, 1f, 0.5f, 0.5f));
+		n_vtx++;
+		Vector2 vert = new Vector2();
+		calcAnglePoint(beginDegrees, vert);
+		addVertToList(lvert, vert);
+		n_vtx++;
+		for (int deg = (int)(Math.ceil(beginDegrees / 45.0) * 45); deg < endDegrees / 45 * 45; deg++) {
+			n_vtx++;
+			calcAnglePoint(deg, vert);
+			addVertToList(lvert, vert);
 		}
-		shaderProgram.begin();
-		shaderProgram.setUniformMatrix("u_projTrans", GameMain.instance.activeStage.getCamera().projection);
-		ResourceManager.barrages.get(245).bind();
-		mesh.setVertices(new float[] {
-				0, 0, 0, 1, 1, 1, 1, 0.5f, 0.5f,
-				-100f, 100f, 0, 1, 1, 1, 1, 0, 1,
-				100f, 100f, 0, 1, 1, 1, 1, 1, 1,
-				100f, -100f, 0, 1, 1, 1, 1, 1, 0,
-				-100f, -100f, 0, 1, 1, 1, 1, 0, 0,
-				-100f, -60f, 0, 1, 1, 1, 1, 0f, 0.2f
-				//-100f, 100f, 0, 1, 1, 1, 1, 0, 1
-		});
-		mesh.setIndices(new short[] {
-				0, 1, 2, 3, 4, 5
-		});
-		mesh.render(shaderProgram, GL20.GL_TRIANGLE_FAN);
-		shaderProgram.end();
+		calcAnglePoint(endDegrees, vert);
+		addVertToList(lvert, vert);
+		n_vtx++;
+		mesh = new Mesh(VertexDataType.VertexArray, false, n_vtx, n_vtx, list.toArray(new VertexAttribute[list.size()]));
+		short[] indices = new short[n_vtx];
+		for (short i = 0; i < n_vtx; i++) indices[i] = i;
+		mesh.setIndices(indices);
+		initialVertices = new float[lvert.size()];
+		operatedVertices = new float[lvert.size()];
+		for (int i = 0; i < initialVertices.length; i++) {
+			initialVertices[i] = lvert.get(i);
+		}
 	}
+	
+	public void addVertToList(List<Float> list, Vector2 vert) {
+		list.addAll(Arrays.asList(vert.x, vert.y, 0f, 1f, 1f, 1f, 1f, (vert.x + 1f) / 2f, (vert.y + 1f) / 2f));
+	}
+	
+	@Override
+	public float getWidth() {
+		return texture.getWidth() * getScaleX();
+	}
+	
+	@Override
+	public float getHeight() {
+		return texture.getHeight() * getScaleY();
+	}
+	
+	public void calcAnglePoint(int degrees, Vector2 out) {
+		degrees = degrees % 360;
+		degrees = degrees < 0 ? degrees + 360 : degrees;
+		if (degrees >= 45 && degrees < 135) {
+			out.y = 1f;
+			out.x = out.y / (float) Math.tan(degrees * MathUtils.degreesToRadians);
+		} else if (degrees >= 135 && degrees < 225) {
+			out.x = -1f;
+			out.y = out.x * (float) Math.tan(degrees * MathUtils.degreesToRadians);
+		} else if (degrees >= 225 && degrees < 315) {
+			out.y = -1f;
+			out.x = out.y / (float) Math.tan(degrees * MathUtils.degreesToRadians);
+		} else {
+			out.x = 1f;
+			out.y = out.x * (float) Math.tan(degrees * MathUtils.degreesToRadians);
+		}
+	}
+	
+	Matrix4 mat4_trans = new Matrix4();
+	
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		shaderProgram = batch.getShader();
+		texture.bind();
+		System.arraycopy(initialVertices, 0, operatedVertices, 0, initialVertices.length);
+		mesh.setVertices(operatedVertices);
+		mat4_trans.setToRotation(0, 0, 1, getRotation());
+		mesh.transform(mat4_trans);
+		mat4_trans.setToTranslationAndScaling(getX(), getY(), 0, getWidth() / 2f, -getHeight() / 2f, 1);
+		mesh.transform(mat4_trans);
+		Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		mesh.render(shaderProgram, GL20.GL_TRIANGLE_FAN);
+	}
+
 }
