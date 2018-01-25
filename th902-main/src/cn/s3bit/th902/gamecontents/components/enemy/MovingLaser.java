eb6871bde4f13b1d52521556a58484f90abecc52
北海若 <shiruoxi2010@163.com>
2018-01-25 10:10:03 +0800

    MovingLaser Judge

--- th902-main/src/cn/s3bit/th902/gamecontents/components/enemy/MovingLaser.java
+++ th902-main/src/cn/s3bit/th902/gamecontents/components/enemy/MovingLaser.java
@@ -1,15 +1,22 @@
 package cn.s3bit.th902.gamecontents.components.enemy;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 
 import cn.s3bit.th902.ResourceManager;
 import cn.s3bit.th902.gamecontents.Entity;
+import cn.s3bit.th902.gamecontents.IJudgeCallback;
+import cn.s3bit.th902.gamecontents.JudgingSystem;
 import cn.s3bit.th902.gamecontents.components.Component;
 import cn.s3bit.th902.gamecontents.components.LaserLikeRenderer;
 import cn.s3bit.th902.gamecontents.components.Transform;
+import cn.s3bit.th902.gamecontents.components.ai.IMovement;
 import cn.s3bit.th902.gamecontents.components.ai.LifeCountDown;
+import cn.s3bit.th902.utils.ImmutableWrapper;
 
 public class MovingLaser extends Component {
 	
@@ -18,37 +25,62 @@ public class MovingLaser extends Component {
 	private int mTimeleft;
 	
 	LinkedList<Vector2> positions = new LinkedList<>();
+	List<ImmutableWrapper<Circle>> judges = new ArrayList<>();
 	
-	public static Entity Create(Vector2 position, int barrageID, int timeleft, int lifetime) {
+	Entity entity;
+	IMovement movement;
+	
+	public static Entity Create(Vector2 position, int barrageID, int timeleft, int lifetime, float judgeSize) {
 		Entity n = Entity.Create();
 		n.AddComponent(new Transform(position));
 		n.AddComponent(new LaserLikeRenderer(ResourceManager.barrages.get(barrageID), timeleft * 6));
 		n.AddComponent(new LifeCountDown(lifetime));
-		n.AddComponent(new MovingLaser(timeleft));
+		n.AddComponent(new MovingLaser(timeleft, judgeSize));
 		return n;
 	}
 	
-	public MovingLaser(int timeleft) {
+	public MovingLaser(int timeleft, float judgeSize) {
 		mTimeleft = timeleft;
+		for (int i=0; i<timeleft; i++)
+			judges.add(ImmutableWrapper.wrap(new Circle(1000, 1000, judgeSize)));
 	}
 
 	@Override
 	public void Initialize(Entity entity) {
+		this.entity = entity;
 		mRenderer = entity.GetComponent(LaserLikeRenderer.class);
 		mTransform = entity.GetComponent(Transform.class);
+		JudgingSystem.clearByBombs.put(mTransform.immutablePosition, entity);
 		Update();
 	}
 
 	@Override
 	public void Update() {
-		if (positions.size() < mTimeleft)
+		if (movement == null)
+			movement = entity.GetComponent(IMovement.class);
+		if (positions.size() < mTimeleft) {
+			JudgingSystem.registerEnemyJudge(judges.get(positions.size()), IJudgeCallback.NONE, movement);
 			positions.add(mTransform.position.cpy());
+		}
 		else {
 			Vector2 recycled = positions.removeFirst();
 			recycled.set(mTransform.position);
 			positions.add(recycled);
 		}
+		int pid = 0;
+		for (Vector2 pos : positions) {
+			judges.get(pid).getData().setPosition(pos);
+			pid++;
+		}
 		mRenderer.laserLikeDrawing.setLaserPoints(positions, null);
+	}
+	
+	@Override
+	public void Kill() {
+		super.Kill();
+		for (ImmutableWrapper<Circle> w : judges) {
+			JudgingSystem.unregisterEnemyJudge(w);
+		}
 	}
 
 }
--- th902-main/src/cn/s3bit/th902/tests/LaserTestScene.java
+++ th902-main/src/cn/s3bit/th902/tests/LaserTestScene.java
@@ -12,7 +12,7 @@ public class LaserTestScene extends DanmakuScene {
 	@Override
 	public void Initialize(Entity entity) {
 		yield.append(() -> {
-			Entity proj = MovingLaser.Create(new Vector2(285, 500), 4, 30, 1200);
+			Entity proj = MovingLaser.Create(new Vector2(285, 500), 4, 30, 1200, 5f);
 			proj.AddComponent(new MoveTracking(3f));
 		});
 	}
