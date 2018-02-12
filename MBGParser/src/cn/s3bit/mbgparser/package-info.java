00b677e48c0c61fe2e8e02c420a1170e28728ef2
北海若 <shiruoxi2010@163.com>
2018-02-12 12:19:29 +0800

    package-info.java

---
+++ MBGParser/src/cn/s3bit/mbgparser/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains the MBG parser.
+ * <br />
+ * This is a Java rewritten version of the open-source library MBGParser (at https://github.com/smallluma/MBGParser).  
+ * <br />
+ * The rest of the TH902 project is under the GPLv3 License, but this part is under MIT License.
+ */
+package cn.s3bit.mbgparser;
