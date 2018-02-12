f2c08c0f842c5c6e3de38c11a0321556438ad2cb
北海若 <shiruoxi2010@163.com>
2018-02-12 12:23:57 +0800

    Unify Indents

--- MBGParser/src/cn/s3bit/mbgparser/MBGUtils.java
+++ MBGParser/src/cn/s3bit/mbgparser/MBGUtils.java
@@ -9,66 +9,65 @@ public final class MBGUtils {
 	}
 	public static String readString(MRef<String> line, char splitter) {
 		if (line == null || isNullOrWhiteSpace(line.data))
-            throw new IllegalArgumentException("无法从空字符串读取信息。");
+			throw new IllegalArgumentException("无法从空字符串读取信息。");
 
-        int spl = line.data.indexOf(splitter);
-        if (spl != -1)
-        {
-            String ret = line.data.substring(0, spl);
-            line.data = line.data.substring(1 + spl);
-            return ret;
-        }
-        else
-        {
-            String ret = line.data;
-            line.data = "";
-            return ret;
-        }
+		int spl = line.data.indexOf(splitter);
+		if (spl != -1) {
+			String ret = line.data.substring(0, spl);
+			line.data = line.data.substring(1 + spl);
+			return ret;
+		}
+		else {
+			String ret = line.data;
+			line.data = "";
+			return ret;
+		}
 	}
+	
 	public static boolean readBool(MRef<String> line) {
 		return readBool(line, ',');
 	}
 	public static boolean readBool(MRef<String> line, char splitter) {
-        return Boolean.parseBoolean(readString(line, splitter));
-    }
+		return Boolean.parseBoolean(readString(line, splitter));
+	}
 	
 	public static long readUInt(MRef<String> line) {
-        return readUInt(line, ',');
-    }
+		return readUInt(line, ',');
+	}
 	public static long readUInt(MRef<String> line, char splitter) {
-        return Long.parseLong(readString(line, splitter));
-    }
+		return Long.parseLong(readString(line, splitter));
+	}
 
 	public static int readInt(MRef<String> line) {
-        return readInt(line, ',');
-    }
-    public static int readInt(MRef<String> line, char splitter) {
-        return Integer.parseInt(readString(line, splitter));
-    }
+		return readInt(line, ',');
+	}
+	public static int readInt(MRef<String> line, char splitter) {
+		return Integer.parseInt(readString(line, splitter));
+	}
 
-    public static float readFloat(MRef<String> line) {
-        return readFloat(line, ',');
-    }
-    public static float readFloat(MRef<String> line, char splitter) {
-        return Float.parseFloat(readString(line, splitter));
-    }
+	public static float readFloat(MRef<String> line) {
+		return readFloat(line, ',');
+	}
+	public static float readFloat(MRef<String> line, char splitter) {
+		return Float.parseFloat(readString(line, splitter));
+	}
+	
+	public static Position<Float> readPosition(MRef<String> line) {
+		return readPosition(line, ',');
+	}
+	public static Position<Float> readPosition(MRef<String> line, char splitter) {
+		String content = readString(line, splitter);
 
-    public static Position<Float> readPosition(MRef<String> line) {
-    	return readPosition(line, ',');
-    }
-    public static Position<Float> readPosition(MRef<String> line, char splitter) {
-        String content = readString(line, splitter);
+		int px1 = content.indexOf(':') + 1;
+		int px2 = content.indexOf('Y');
 
-        int px1 = content.indexOf(':') + 1;
-        int px2 = content.indexOf('Y');
+		int py1 = content.lastIndexOf(':') + 1;
+		int py2 = content.lastIndexOf('}');
 
-        int py1 = content.lastIndexOf(':') + 1;
-        int py2 = content.lastIndexOf('}');
+		Position<Float> p = new Position<>();
+		p.x = Float.parseFloat(content.substring(px1, px2).trim());
+		p.y = Float.parseFloat(content.substring(py1, py2).trim());
 
-        Position<Float> p = new Position<>();
-        p.x = Float.parseFloat(content.substring(px1, px2).trim());
-        p.y = Float.parseFloat(content.substring(py1, py2).trim());
-
-        return p;
-    }
+		return p;
+	}
 }
