f39e4be77ae74b359ac5762bd12fbfe4b62ad913
北海若 <shiruoxi2010@163.com>
2018-02-13 10:50:57 +0800

    Unify Indents

--- MBGParser/src/cn/s3bit/mbgparser/Center.java
+++ MBGParser/src/cn/s3bit/mbgparser/Center.java
@@ -7,33 +7,33 @@ import static cn.s3bit.mbgparser.MBGUtils.*;
 
 public class Center {
 	public Position<Float> Position;
-    public Motion<Float> Motion;
+	public Motion<Float> Motion;
 
-    public List<Event> Events;
+	public List<Event> Events;
 
-    public static Center parseFromContent(String contentRaw)
-    {
-        if (contentRaw.equalsIgnoreCase("False"))
-            return null;
-        else
-        {
-            Center center = new Center();
+	public static Center parseFromContent(String contentRaw)
+	{
+		if (contentRaw.equalsIgnoreCase("False"))
+			return null;
+		else
+		{
+			Center center = new Center();
 
-            MRef<String> content = new MRef<String>(contentRaw);
-            center.Position.x = Float.parseFloat(readString(content));
-            center.Position.y = Float.parseFloat(readString(content));
+			MRef<String> content = new MRef<String>(contentRaw);
+			center.Position.x = Float.parseFloat(readString(content));
+			center.Position.y = Float.parseFloat(readString(content));
 
-            center.Motion.speed = Float.parseFloat(readString(content));
-            center.Motion.speedDirection = Float.parseFloat(readString(content));
+			center.Motion.speed = Float.parseFloat(readString(content));
+			center.Motion.speedDirection = Float.parseFloat(readString(content));
 
-            center.Motion.acceleration = Float.parseFloat(readString(content));
-            center.Motion.accelerationDirection = Float.parseFloat(readString(content));
+			center.Motion.acceleration = Float.parseFloat(readString(content));
+			center.Motion.accelerationDirection = Float.parseFloat(readString(content));
 
-            center.Events = null;
-            if (!contentRaw.equals(""))
-                center.Events = Event.parseEvents(readString(content));
+			center.Events = null;
+			if (!contentRaw.equals(""))
+				center.Events = Event.parseEvents(readString(content));
 
-            return center;
-        }
-    }
+			return center;
+		}
+	}
 }
