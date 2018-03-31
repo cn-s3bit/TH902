12ded60f7fa9f41a380f70f683ae22324185a71f
北海若 <shiruoxi2010@163.com>
2018-03-31 09:52:25 +0800

    Audio Manager class

---
+++ th902-main/src/cn/s3bit/th902/AudioManager.java
@@ -0,0 +1,24 @@
+package cn.s3bit.th902;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.files.FileHandle;
+
+public class AudioManager {
+	public static Music music = null;
+	public static void PlayBGM(FileHandle handle, boolean loop) {
+		if (music != null) {
+			music.stop();
+			music.dispose();
+		}
+		music = Gdx.audio.newMusic(Gdx.files.internal("resources/BGM/Starting Scene.mp3"));
+		music.setLooping(loop);
+		music.play();
+	}
+	public static void PauseBGM() {
+		if (music != null) music.pause();
+	}
+	public static void StopBGM() {
+		if (music != null) music.stop();
+	}
+}
