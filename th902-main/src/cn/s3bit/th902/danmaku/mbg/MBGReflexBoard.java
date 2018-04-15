673575b729866f90f057af0e6844c486bde4edae
北海若 <shiruoxi2010@163.com>
2018-04-15 11:00:46 +0800

    Reflex Board Events

--- th902-main/src/cn/s3bit/th902/danmaku/mbg/MBGReflexBoard.java
+++ th902-main/src/cn/s3bit/th902/danmaku/mbg/MBGReflexBoard.java
@@ -6,8 +6,12 @@ import com.badlogic.gdx.utils.ObjectIntMap;
 import com.badlogic.gdx.utils.ObjectIntMap.Entries;
 import com.badlogic.gdx.utils.ObjectIntMap.Entry;
 
+import cn.s3bit.mbgparser.event.DataOperateAction;
+import cn.s3bit.mbgparser.event.ReflexBoardAction;
+import cn.s3bit.mbgparser.event.DataOperateAction.TweenFunctionType;
 import cn.s3bit.mbgparser.item.ReflexBoard;
 import cn.s3bit.th902.danmaku.mbg.condition.IConditionJudger;
+import cn.s3bit.th902.danmaku.mbg.event.BulletEvents;
 import cn.s3bit.th902.danmaku.mbg.event.IEventFirer;
 import cn.s3bit.th902.danmaku.mbg.event.ILValueProvider;
 import cn.s3bit.th902.gamecontents.components.enemy.EnemyJudgeCircle;
@@ -93,14 +97,13 @@ public class MBGReflexBoard extends AbstractMBGComponent<ReflexBoard> {
 		if (hitCount.get(bullet.judgeCircle, 0) >= mbgItem.次数) {
 			return false;
 		}
-		if (hitCoolDown.get(bullet.judgeCircle, 0) > 0) {
-			if (hitCoolDown.getAndIncrement(bullet.judgeCircle, 0, -1) <= 1)
-				hitCoolDown.remove(bullet.judgeCircle, 0);
+		if (hitCoolDown.get(bullet.judgeCircle, 0) >= 0) {
+			hitCoolDown.getAndIncrement(bullet.judgeCircle, 0, -1);
 			return false;
 		}
 		if (judgeLine.intersectCircle(bullet.judgeCircle.circle)) {
 			hitCount.getAndIncrement(bullet.judgeCircle, 0, 1);
-			hitCoolDown.put(bullet.judgeCircle, 5);
+			hitCoolDown.getAndIncrement(bullet.judgeCircle, 0, 5);
 			return true;
 		}
 		return false;
@@ -110,5 +113,18 @@ public class MBGReflexBoard extends AbstractMBGComponent<ReflexBoard> {
 	public void manipulateBullet(MBGBullet bullet) {
 		float in = bullet.moveBasic.velocity.angle(direction);
 		bullet.moveBasic.velocity.setAngle(direction.angle() + in); // angle(out) = angle(in)
+		
+		for (ReflexBoardAction action : mbgItem.碰撞事件组) {
+			DataOperateAction dataOperateAction = new DataOperateAction();
+			dataOperateAction.LValue = action.LValue;
+			dataOperateAction.RValue = action.RValue;
+			dataOperateAction.TweenFunction = TweenFunctionType.Fixed;
+			dataOperateAction.TweenTime = 1;
+			dataOperateAction.Operator = action.Operator;
+			dataOperateAction.Times = 9999;
+			MBGEventTask mbgEventTask = new MBGEventTask(1, dataOperateAction, bullet);
+			mbgEventTask.timeLeft = 0;
+			BulletEvents.instance.fireDataOperation(bullet, mbgEventTask);
+		}
 	}
 }
