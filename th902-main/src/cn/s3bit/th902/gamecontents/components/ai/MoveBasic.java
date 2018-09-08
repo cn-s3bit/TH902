c147283412ec171e6efcd32021e8262a99b78ae3
北海若 <shiruoxi2010@163.com>
2018-09-08 10:18:19 +0800

    Speed Behavior Change

--- th902-main/src/cn/s3bit/th902/gamecontents/components/ai/MoveBasic.java
+++ th902-main/src/cn/s3bit/th902/gamecontents/components/ai/MoveBasic.java
@@ -10,6 +10,8 @@ public class MoveBasic extends Component implements IMovement {
 	protected Transform transform;
 	public Vector2 velocity;
 	public Vector2 acc;
+	
+	private Vector2 mInnerAccSpd;
 	public MoveBasic(float x, float y) {
 		this(x, y, 0, 0);
 	}
@@ -19,6 +21,7 @@ public class MoveBasic extends Component implements IMovement {
 	public MoveBasic(float x, float y, float ax, float ay) {
 		acc = new Vector2(ax, ay);
 		velocity = new Vector2(x, y);
+		mInnerAccSpd = new Vector2(0f, 0f);
 	}
 	public MoveBasic(Vector2 vel, Vector2 accel) {
 		this(vel.x, vel.y, accel.x, accel.y);
@@ -29,13 +32,14 @@ public class MoveBasic extends Component implements IMovement {
 		transform = entity.GetComponent(Transform.class);
 	}
 
+	
 	@Override
 	public void Update() {
-		velocity.add(acc);
-		transform.position.add(velocity);
+		mInnerAccSpd.add(acc);
+		transform.position.add(velocity).add(mInnerAccSpd);
 	}
 	@Override
 	public Vector2 getCurrentVelocity() {
-		return velocity;
+		return IMoveFunction.vct2_tmp1.set(velocity).add(mInnerAccSpd);
 	}
 }
