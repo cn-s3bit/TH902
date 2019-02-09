723d2407afb2dab5738a0b091d475512764abbbd
北海若 <shiruoxi2010@163.com>
2019-02-09 17:46:50 +0800

    Atomic Parallelism

--- th902-main/src/cn/s3bit/th902/gamecontents/JudgingSystem.java
+++ th902-main/src/cn/s3bit/th902/gamecontents/JudgingSystem.java
@@ -3,6 +3,7 @@ package cn.s3bit.th902.gamecontents;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
 
 import com.badlogic.gdx.math.Ellipse;
@@ -18,7 +19,7 @@ public class JudgingSystem {
 	public static HashMap<ImmutableWrapper<Ellipse>, PlayerCollisionData> enemyJudges = new HashMap<>();
 	public static HashMap<ImmutableWrapper<LineSegment>, IJudgeCallback> friendlyJudges = new HashMap<>();
 	public static HashMap<ImmutableWrapper<Vector2>, IJudgeCallback> chaseableEnemyPositions = new HashMap<>();
-	private static Entry<ImmutableWrapper<Ellipse>, PlayerCollisionData> mJudgeEntry;
+	private static AtomicReference<Entry<ImmutableWrapper<Ellipse>, PlayerCollisionData> > mJudgeEntry = new AtomicReference<>(null);
 	public static HashMap<ImmutableWrapper<Vector2>, Entity> clearByBombs = new HashMap<>();
 	
 	public static class PlayerCollisionData {
@@ -36,7 +37,7 @@ public class JudgingSystem {
 	
 	public static PlayerCollisionData playerCollision() {
 		Stream<Entry<ImmutableWrapper<Ellipse>, PlayerCollisionData>> stream = enemyJudges.entrySet().parallelStream();
-		mJudgeEntry = null;
+		mJudgeEntry.set(null);
 		stream.forEach((entry) -> {
 			if (entry.getValue().judgeCallback.getDamage() <= 0 || Math.max(entry.getKey().getData().width, entry.getKey().getData().height) < 0.1f) {
 				return;
@@ -44,11 +45,11 @@ public class JudgingSystem {
 			Vector2 tmp = new Vector2(playerJudge);
 			tmp.sub(entry.getKey().getData().x, entry.getKey().getData().y).rotate(-entry.getValue().transform.rotation).add(entry.getKey().getData().x, entry.getKey().getData().y);
 			if (entry.getKey().getData().contains(tmp)) {
-				mJudgeEntry = entry;
+				mJudgeEntry.getAndSet(entry);
 			}
 		});
 		stream.close();
-		return mJudgeEntry == null ? null : mJudgeEntry.getValue();
+		return mJudgeEntry.get() == null ? null : mJudgeEntry.get().getValue();
 	}
 	
 	public static IJudgeCallback collideFriendlyBullets(Ellipse judge, float rotation) {
